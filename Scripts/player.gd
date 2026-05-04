c99d5296e3d8be1761f66cf14a967f4fb5d47750
Lisa.Li <lisalikebook@gmail.com>
2026-05-04 11:12:52 -0700

    5/4

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,37 +1,42 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY =-850
+const JUMP_VELOCITY = -850.0
 var alive = true
+var can_move = true
 
 func _physics_process(delta: float) -> void:
 	if !alive:
 		return
-	# add animation
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "run"
-	if velocity.x !=0:
-		$AnimatedSprite2D.animation = "run"
-	else:
-		$AnimatedSprite2D.animation ="idle"
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		$AnimatedSprite2D.animation = "jump"
 	# Handle jump. 
-	if Input .is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		$JumpSound.play()
-	var direction := Input.get_axis("left", "right")
-	if direction == 1.0:
-		$AnimatedSprite2D.flip_h = false
-	if direction == -1.0:
-		$AnimatedSprite2D.flip_h = false
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if can_move:
+		if Input .is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			$JumpSound.play()
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+		move_and_slide()
+			
+			
+		if velocity.x !=0:
+			$AnimatedSprite2D.animation = "run"
+		else:
+			$AnimatedSprite2D.animation ="idle"
 		
-	move_and_slide()
+		if direction == 1.0:
+			$AnimatedSprite2D.flip_h = false
+		elif direction == -1.0:
+			$AnimatedSprite2D.flip_h = false
+		
+
 func die() -> void:
 	$DeathSound.play()
 	$AnimatedSprite2D.animation = "hit"
--- level_02.gd
+++ level_02.gd
@@ -5,18 +5,23 @@ var current_level_root: Node = null
 
 func _ready() -> void:
 	#_setup_level()
-	#$HUD/Fade.modulate.a = 1.0
+	fade = $HUD/Fade
+	fade.modulate.a = 1.0
 	current_level_root = get_node("LevelRoot")
-	#_load_level(level)
-func _process(delta: float) -> void:
-	pass
-	
+	_load_level(level, ture, false)
 	#------------------------
 	# Event Handles
 	#------------------------
+func _process(delta: float) -> void:
+	pass
 	
-func _load_level(level_number:int) -> void:
-	#await _fade(1.0)
+func _load_level(level_number:int, first_load:bool) -> void:
+	# fade first_load:
+	if not first_load:
+		await _fade(1.0)
+	if reset_score:
+		score = 0
+		$HUD/scoreLabel.text = "score: " + Str(score)
 	if current_level_root:
 		current_level_root.queue_free()
 		#change level
@@ -26,15 +31,18 @@ func _load_level(level_number:int) -> void:
 	current_level_root.name = "LevelRoot"
 	_setup_level(current_level_root)
 	#Fade in
-	#await _fade(0.0)
+	await _fade(0.0)
 	
 func _on_player_died(body):
 	print(body.get_name() + " killed!!!!")
 	body.die()
-	
+	await _load_level(level, false, true)
 func _setup_level() -> void:
-	var exit = $LevelRoot.get_node_or_null("Exit")
-	var apples = $LevelRoot.get_node_or_null("Apples")
+	var exit = current_level_root.get_node_or_null("Exit")
+	if exit:
+		exit.body_entered.connect(_on_exit_body_entered)
+		#connect Apples
+	var apples = current_level_root.get_node_or_null("Apples")
 	if apples:
 		for apple in apples.get_children():
 			apple.collected.connect(increase_score)
@@ -51,14 +59,13 @@ func increase_score() -> void:
 	#peint(score)
 	$HUD/ScoreLabel.text = "Score: %s" % score
 func _on_exit_body_entered(body: Node2D) -> void:
-	if body.name == "Player": 
-		level +=1
-		print("Body: " + body.name + "\nLevel: " + str(level))
-	#body.can_move = false
-	#call_deferred("_load_level", level)
-
+	if body.name == "Player":
+	level +=1
+	print("Body: " + body.name + "\nLevel: " + str(level))
+	body.can_move = false
+	await _load_level(level, false, false)
 func _fade(to_alpha: float) -> void:
 	var tween := create_tween()
-	#tween.tween_property(fade, "modulate:a", to_alpha, 1.5)
+	tween.tween_property(fade, "modulate:a", to_alpha, 1.5)
 	await tween.finished
 	
